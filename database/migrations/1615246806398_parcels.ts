491dee81e81a4db548eb027255b64ac4c08ddd7e
Ridwan ONIKOYI <onikoyiridwan@gmail.com>
2021-03-10 15:26:47 +0100

    update parcel model

--- database/migrations/1615246806398_parcels.ts
+++ database/migrations/1615246806398_parcels.ts
@@ -1,28 +1,29 @@
-import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import BaseSchema from '@ioc:Adonis/Lucid/Schema';
 
 export default class Parcels extends BaseSchema {
-  protected tableName = 'parcels'
+  protected tableName = 'parcels';
 
-  public async up () {
+  public async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.increments('id')
-      table.integer('placedBy').unsigned().notNullable()
-      table.foreign('placedBy').references('id').inTable('users')
-      table.float('weight').unsigned().notNullable()
-      table.string('weightmetric', 255).notNullable()
-      table.timestamp('sentOn').notNullable()
-      table.timestamp('deliveredOn')
-      table.enu('status', ['placed', 'transiting', 'delivered']).notNullable()
-      table.string('from', 255).notNullable()
-      table.string('to', 255).notNullable()
-      table.string('currentLocation', 255)
+      table.increments('id');
+      table.integer('placedBy').unsigned().notNullable();
+      table.foreign('placedBy').references('id').inTable('users');
+      table.float('weight').unsigned().notNullable();
+      table.string('weightmetric', 255).notNullable();
+      table.timestamp('sentOn').notNullable();
+      table.timestamp('deliveredOn');
+      table
+        .enu('status', ['placed', 'cancelled', 'transiting', 'delivered'])
+        .notNullable();
+      table.string('from', 255).notNullable();
+      table.string('to', 255).notNullable();
+      table.string('currentLocation', 255);
 
-
-      table.timestamps(true)
-    })
+      table.timestamps(true);
+    });
   }
 
-  public async down () {
-    this.schema.dropTable(this.tableName)
+  public async down() {
+    this.schema.dropTable(this.tableName);
   }
 }
